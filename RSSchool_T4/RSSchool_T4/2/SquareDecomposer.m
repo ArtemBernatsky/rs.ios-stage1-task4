a65c216e5319da38284080df553437f44649e251
Artem Bernatsky <bernatskyhome@gmail.com>
2020-04-12 20:55:04 +0300

    feat: task 2

--- RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
+++ RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
@@ -2,7 +2,39 @@
 
 @implementation SquareDecomposer
 - (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number {
-    // good luck
-    return @[@0];
+    NSNumber *longNumber = [NSNumber numberWithLong: [number longValue]];
+    NSNumber *remain = [NSNumber numberWithLong: pow([number longLongValue], 2)];
+
+    NSMutableArray *decomposedArray = [self target: longNumber squaredTarget: remain];
+
+    if (decomposedArray == nil) {
+        return nil;
+    } else {
+        [decomposedArray removeLastObject];
+        return decomposedArray;
+    }
+}
+
+- (NSMutableArray *)target:(NSNumber *)prevNumber squaredTarget:(NSNumber *)numberSquared {
+    if ([numberSquared longValue] == 0) {
+        return [NSMutableArray arrayWithObject: prevNumber];
+    }
+
+    for(long i = [prevNumber longValue] - 1; i > 0; i--) {
+        long prevSquared = pow(i, 2);
+
+        if(([numberSquared longLongValue] - prevSquared) >= 0){
+            NSNumber *nextNumber = [NSNumber numberWithLong: i];
+            NSNumber *nextRemain = [NSNumber numberWithLong:([numberSquared longValue] - prevSquared)];
+            
+            NSMutableArray *result = [self target: nextNumber squaredTarget: nextRemain];
+            if (result != nil){
+                [result addObject: prevNumber];
+                return result;
+            }
+        }
+    }
+    
+    return nil;
 }
 @end
--- RSSchool_T4/RSSchool_T4/3/TelephoneFinder.m
+++ RSSchool_T4/RSSchool_T4/3/TelephoneFinder.m
@@ -2,7 +2,35 @@
 
 @implementation TelephoneFinder
 - (NSArray <NSString*>*)findAllNumbersFromGivenNumber:(NSString*)number {
-    // good luck
-    return @[@"0"];
+    NSMutableArray *result = [NSMutableArray array];
+    NSDictionary * neighbors =
+    @{
+        @"0": @[@"8"],
+        @"1": @[@"2", @"4"],
+        @"2": @[@"1", @"3", @"5"],
+        @"3": @[@"2", @"6"],
+        @"4": @[@"1", @"5", @"7"],
+        @"5": @[@"2", @"4", @"6", @"8"],
+        @"6": @[@"3", @"5", @"9"],
+        @"7": @[@"4", @"8"],
+        @"8": @[@"0", @"5", @"7", @"9"],
+        @"9": @[@"6", @"8"]
+    };
+    
+    if (number.length < 1 || [number intValue] < 0) {
+        return nil;
+    }
+    
+    for (int i = 0; i < number.length; i++) {
+        NSRange range = NSMakeRange(i, 1);
+        NSString *currentChar = [number substringWithRange: range];
+        NSArray *dict = [neighbors objectForKey: currentChar];
+        for (NSString *value in dict) {
+            NSString *currentNeighbors = [number stringByReplacingCharactersInRange: range withString:value];
+            [result addObject: currentNeighbors];
+        }
+    }
+    
+    return result;
 }
 @end
